cae6147b92291aa620c5b51b0e0c4c7abf60b4df
Alexandru Cohal <alexandru.cohal@yahoo.com>
2021-01-17 20:39:15 +0100

    Initialize the Serial data transmission and Connect to the WiFi network

--- code/wfdOplaNode/wfdOplaNode.ino
+++ code/wfdOplaNode/wfdOplaNode.ino
@@ -1,6 +1,28 @@
-void setup() {
+#include <WiFiNINA.h>
+
+#include "arduino_secrets.h"
+
+void setup() 
+{
   // put your setup code here, to run once:
 
+  // Initialize the Serial data transmission
+  Serial.begin(9600);
+  while (!Serial);
+
+  // Connect to the WiFi network
+  int status = WL_IDLE_STATUS;
+  while (status != WL_CONNECTED) 
+  {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    
+    status = WiFi.begin(SECRET_SSID, SECRET_PASS);
+    
+    // Wait 1 seconds for connection
+    delay(1000);
+  }
+  Serial.println("Connected to wifi");
 }
 
 void loop() {
