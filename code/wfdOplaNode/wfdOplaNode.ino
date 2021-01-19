5f823c5ee64fff8eb3b97ea06a06881b53a57d0f
Alexandru Cohal <alexandru.cohal@yahoo.com>
2021-01-19 22:24:15 +0100

    Opla board (i.e. Arduino MKR IoT carrier) declared and initialized

--- code/wfdOplaNode/wfdOplaNode.ino
+++ code/wfdOplaNode/wfdOplaNode.ino
@@ -1,11 +1,12 @@
 #include <WiFiNINA.h>
+#include <Arduino_MKRIoTCarrier.h>
 
 #include "arduino_secrets.h"
 
+MKRIoTCarrier carrier;
+
 void setup() 
 {
-  // put your setup code here, to run once:
-
   // Initialize the Serial data transmission
   Serial.begin(9600);
   while (!Serial);
@@ -23,9 +24,13 @@ void setup()
     delay(1000);
   }
   Serial.println("Connected to wifi");
+
+  //Initialize the MKR IoT Carrier and output any errors in the serial monitor
+  CARRIER_CASE = true;
+  carrier.begin();
+  carrier.display.setRotation(0);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() 
+{
 }
